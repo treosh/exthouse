57d953b7b5d9ec8d0619fda35ac188507719ee23
Artem Denysov <denysov.artem@gmail.com>
2019-03-28 18:21:27 +0200

    Adds more FF extensions

--- script/execute-with-puppeteer.js
+++ script/execute-with-puppeteer.js
@@ -1,11 +1,8 @@
 const ora = require('ora')
-const wunderbar = require('@gribnoysup/wunderbar')
-const percentile = require('percentile')
 const webExt = require('web-ext').default
 const throttle = require('@sitespeed.io/throttle');
-const inRange = require('in-range')
+const getPort = require('get-port')
 const pMap = require('p-map')
-const eol = require('os').EOL
 const { emptyDir } = require('fs-extra')
 const PP = require('puppeteer')
 const PP_FF = require('puppeteer-firefox')
@@ -14,7 +11,7 @@ const { median } = require('simple-statistics')
 const { URL } = require('url')
 const { join } = require('path')
 const { writeFileSync } = require('fs')
-const { unzipExtensions, log } = require('./utils')
+const { unzipExtensions, log, drawChart } = require('./utils')
 const { tmpDir, resultsDir, totalRuns, lhConfig, chromeExtensions, firefoxExtensions, browsers } = require('./settings')
 
 const measureExtensionInChrome = async ({ extension, extName, url, extPath }) => {
@@ -48,7 +45,8 @@ const measureExtensionInFirefox = async ({ extension, extName, url, extPath }) =
   let browser
 
   if (extPath) {
-    const CDPPort = 6006
+    // @todo wait for https://github.com/sindresorhus/get-port/pull/28 and than use range for 6000 port
+    const CDPPort = await getPort()
 
     extensionRunners = await webExt.cmd.run({
       sourceDir: extPath,
@@ -188,75 +186,6 @@ async function main(url, options = {}) {
   spinner.succeed()
 
   return results
-}
-
-const drawChart = (results, options) => {
-  const data = results.slice(0)
-  data.sort(function (a, b) {
-    return a.tti - b.tti
-  })
-
-  const { lmargin, width, xlabel, xmin, xmax } = options
-
-  const normalizedData = data.map(value => {
-    const { tti, name } = value
-    const percentile100 = percentile(100, data, item => item.tti)
-    const percentile65 = percentile(65, data, item => item.tti)
-    const percentile40 = percentile(40, data, item => item.tti)
-    const percentile20 = percentile(20, data, item => item.tti)
-    let color
-
-    if (inRange(tti, percentile100.tti, percentile65.tti)) {
-      color = 'red'
-    } else if (inRange(tti, percentile40.tti, percentile65.tti)) {
-      color = '#ff8300'
-    } else if (inRange(tti, percentile20.tti)) {
-      color = 'green'
-    }
-
-    return {
-      value: tti,
-      label: name,
-      color,
-    }
-  })
-
-  const { __raw } = wunderbar(normalizedData, {
-    min: xmin,
-    max: xmax,
-    length: width,
-    format: '0,000',
-  })
-
-  const { normalizedValues, minValueFormatted, maxValueFormatted } = __raw
-
-  const yAxis = '│'
-  const xAxis = '─'
-  const corner = '╰'
-
-  const padding = ' '.repeat(lmargin)
-
-  const chart = normalizedValues
-    .reverse()
-    .map(value => {
-      const pad = lmargin - value.label.length
-      const paddedLabel = ' '.repeat(pad > 0 ? pad : 0) + value.label
-
-      return `${paddedLabel} ${yAxis}${value.coloredChartBar} ${value.formattedValue}`
-    })
-    .join(`${eol}${padding} ${yAxis}${eol}`)
-
-  const chartTop = `${padding} ${yAxis}`
-
-  const chartBottom = `${padding} ${corner}${xAxis.repeat(width)}`
-
-  const labelPadding = ' '.repeat(Math.max(0, (width - xlabel.length - 2) / 2))
-
-  const chartScale = `${padding}  ${minValueFormatted}${labelPadding}${xlabel}${labelPadding}${maxValueFormatted}`
-
-  console.log('')
-  console.log([chartTop, chart, chartBottom, chartScale].join(eol))
-  console.log('')
 }
 
 exports.extensions = main
