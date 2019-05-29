23fc8e3c5c416cc56b951874b8f06a7d37ca31d5
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2019-05-29 17:44:59 +0200

    basic options

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,41 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const { join } = require('path')
+const { existsSync } = require('fs')
 const { launch } = require('../src')
 const { browsers } = require('../src/settings')
 const { version } = require('../package.json')
 
 program
-  .command('ext-perf [<extDir>]')
-  .usage('EXTENSION_DIR --url https://treo.sh/')
+  .name('ext-perf')
+  .usage('[path/to/extension.crx] [options]')
+  .option('-f, --folder <dir>', 'analyze the folder with extensions')
+  .option('-r, --runs <number>', 'amount of runs to evaluate median performance value', '5')
+  .option('-u, --url <url>', 'url to evaluate extension performance', 'https://example.com/')
+  .option('-o, --output <format>', 'output format options: [cli, json]', 'json')
   .version(version)
-  .option('--url <url>', 'url to test extension', 'https://example.com/')
-  .option('--json', 'output results in json format')
-  .option('--browserType <type>', 'specify the type of browser', browsers.CHROME)
-  .action((dir = '', cmd) => {
-    launch(dir, cmd)
-      .catch(e => {
-        console.error(e)
-        process.exit(1)
-      })
-      .then(() => process.exit())
-  })
 
 program.parse(process.argv)
+
+if (!program.args[0] && !program.folder) {
+  console.error('')
+  console.error('Error: provide a path to extension or a folder containing extensions.')
+  console.error('')
+  console.error('Examples:')
+  console.error('')
+  console.error('   ext-perf path/to/extension.crx')
+  console.error('   ext-perf --folder=path/to/extension')
+  console.error('')
+  process.exit(1)
+}
+
+const path = join(process.cwd(), program.args[0])
+const opts = { output: program.output, folder: program.folder, url: program.url, runs: parseInt(program.runs) }
+
+if (!existsSync(path)) {
+  console.error(`Invalid path to extension: ${path}`)
+  process.exit(1)
+}
+
+console.log(path, opts)
